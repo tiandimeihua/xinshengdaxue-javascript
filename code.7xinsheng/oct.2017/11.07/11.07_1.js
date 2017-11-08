4e0479059f67481dad1407aa816968cbc3f419b4
tiandimeihua <25058977+tiandimeihua@users.noreply.github.com>
2017-11-07 22:37:28 -0500

    update 11.07

---
+++ code.7xinsheng/oct.2017/11.07/11.07_1.js
@@ -0,0 +1,38 @@
+function testVO(name, age) {
+  console.log(a);
+
+  console.log(b);
+
+  console.log(c);
+  var a = 1;
+
+  var b = function() {
+    return 2;
+  };
+
+  function c() {
+    return 3;
+  }
+
+  var c = 4;
+
+  //the second time try;
+  /*
+  console.log(a);
+
+  console.log(b);
+
+  console.log(c);
+  */
+}
+
+testVO("leslie", 21);
+
+//the first time try;
+/*
+  console.log(a);
+
+  console.log(b);
+
+  console.log(c);
+  */
